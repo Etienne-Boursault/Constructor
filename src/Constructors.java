bb54c312518a0720679bda08c7e4f6df811b0460
Etienne Boursault <43215408+EhBoss@users.noreply.github.com>
2022-06-19 13:11:32 +0200

    First commit

---
+++ src/Constructors.java
@@ -0,0 +1,9 @@
+public class Constructors {
+
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog("Jerry");
+        System.out.println(myDog.name);
+        System.out.println(Constants.NUMBER_OF_MONTHS_IN_A_YEAR);
+    }
+}
